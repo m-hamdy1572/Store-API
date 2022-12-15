abaa459bd243be9858715130b392da3e6bcc31cc
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2022-12-15 16:39:54 +0200

    Update app.js
    r

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use( (req, res, next)=> {
 });
 
 app.get('/', (req, res) => {
-    res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>')
+    res.send('<h1>Store API</h1><a href="/api/v1/getAllProducts">Products Route</a>')
 })
 
 app.use('/api/v1', productsRouter);
